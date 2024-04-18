8af61daf31fd0979c56d95f85e664f1fc5f742da
Kaio <Kaiovndias30@gmail.com>
2024-04-17 22:58:00 -0300

    Inseri a quinta questão.

---
+++ Dev Software II/LISTA-1/questao5.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class questao5 {
+    public static void main(String[] args) {
+        String nomeArquivoEntrada = "meuarquivo.txt";
+        String nomeArquivoSaida = "meuarquivo_python.txt";
+
+        try {
+            FileReader arquivoReader = new FileReader(nomeArquivoEntrada);
+            BufferedReader bufferedReader = new BufferedReader(arquivoReader);
+
+            FileWriter arquivoWriter = new FileWriter(nomeArquivoSaida);
+            BufferedWriter bufferedWriter = new BufferedWriter(arquivoWriter);
+
+            String linha;
+            while ((linha = bufferedReader.readLine()) != null) {
+                linha = linha.replaceAll("Java", "Python");
+                bufferedWriter.write(linha);
+                bufferedWriter.newLine();
+            }
+
+            bufferedReader.close();
+            bufferedWriter.close();
+            System.out.println("Substituição concluída. Resultado salvo em " + nomeArquivoSaida);
+        } catch (IOException e) {
+            System.out.println("Erro ao processar o arquivo: " + e.getMessage());
+        }
+    }
+}
