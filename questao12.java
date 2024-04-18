94c0cfc5c3cd043f31b0c1c7d44a45cc5dce6e9f
Kaio <Kaiovndias30@gmail.com>
2024-04-18 14:21:02 -0300

    Inseri a décima segunda questão.

--- questao12.java
+++ questao12.java
@@ -1,3 +1,14 @@
+import java.io.*;
+
 public class questao12 {
-    
+    public static void main(String[] args) {
+        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("pessoa.dat"))) {
+            Pessoa pessoa = (Pessoa) inputStream.readObject();
+            System.out.println("Detalhes da Pessoa:");
+            System.out.println("Nome: " + pessoa.getNome());
+            System.out.println("Idade: " + pessoa.getIdade());
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
