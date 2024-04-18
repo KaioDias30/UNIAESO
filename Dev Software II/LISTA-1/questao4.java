eb75793a0ce62fa95ff10bd87105ccdfe36bcc06
Kaio <Kaiovndias30@gmail.com>
2024-04-17 22:53:30 -0300

    Inseri a quarta questão.

---
+++ Dev Software II/LISTA-1/questao4.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class questao4 {
+    public static void main(String[] args) {
+        String nomeArquivo = "meuarquivo.txt";
+        int contadorLinhas = 0;
+
+        try {
+            FileReader arquivoReader = new FileReader(nomeArquivo);
+            BufferedReader bufferedReader = new BufferedReader(arquivoReader);
+
+            while (bufferedReader.readLine() != null) {
+                contadorLinhas++;
+            }
+
+            bufferedReader.close();
+            System.out.println("Número de linhas no arquivo: " + contadorLinhas);
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+        }
+    }
+}
