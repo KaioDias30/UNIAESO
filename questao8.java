d7f5e5a6e13581ea3977067a67b4fc40c19d493d
Kaio <Kaiovndias30@gmail.com>
2024-04-18 14:05:29 -0300

    Inseri a oitava questão.

--- questao8.java
+++ questao8.java
@@ -1,3 +1,21 @@
+import java.io.*;
+
 public class questao8 {
-    
+    public static void main(String[] args) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("meuarquivo.txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("meuarquivo_sem_excluir.txt"));
+            String linha;
+            while ((linha = reader.readLine()) != null) {
+                if (!linha.contains("excluir")) {
+                    writer.write(linha);
+                    writer.newLine();
+                }
+            }
+            reader.close();
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
 }
